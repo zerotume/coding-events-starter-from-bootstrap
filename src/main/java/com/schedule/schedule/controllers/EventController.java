2b00ef62fab37c59c1b10829942a6c5785f208f6
zerotume <zerotume@gmail.com>
2023-02-15 15:55:31 -0500

    first commit from starter

---
+++ src/main/java/com/schedule/schedule/controllers/EventController.java
@@ -0,0 +1,41 @@
+package com.schedule.schedule.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Chris Bay
+ */
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    private static List<String> events = new ArrayList<>();
+
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        model.addAttribute("title", "All Events");
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+
+    @GetMapping("create")
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
+        return "events/create";
+    }
+
+    @PostMapping("create")
+    public String processCreateEventForm(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
+    }
+
+}
---
+++ src/main/java/com/schedule/schedule/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.schedule.schedule.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by Chris Bay
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+}
